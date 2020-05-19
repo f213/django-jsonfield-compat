b1f1955077041fe9f14eb32be7bbcc6a7525e366
Fedor Borshev <fedor@borshev.com>
2020-05-19 21:34:03 +0300

    Changed project name to correctly include it into forks

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name="django-jsonfield-compat",
+    name="django-jsonfield-compat-with-django30",
     version='0.4.5',
     description="Compatability layer between django-jsonfield and Django's native JSONField",
     long_description=open("README.md").read(),
-    url="http://github.com/kbussell/django-jsonfield-compat/",
+    url="http://github.com/f213/django-jsonfield-compat/",
     author="Keith Bussell",
     author_email="kbussell@gmail.com",
     packages=[
